c802127469cbd7de44f80c07e90049330414f488
dorjoo <dorjoohover@gmail.com>
2025-11-02 13:26:22 +0800

    a

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -207,11 +207,13 @@ export class OrderService {
             };
           }),
         );
+        console.log(occupiedSlots);
         schedules.forEach((slot) => {
           const { index, times } = slot; // day = 0–6, times = "10|11|12"
 
           // тухайн slot-ууд orders-тэй давхцаж байгааг filter
           const freeTimes = times.filter((hour) => {
+            console.log(hour);
             return !occupiedSlots.some(
               (o) =>
                 o.day === index && hour >= o.start_time && hour < o.end_time,
